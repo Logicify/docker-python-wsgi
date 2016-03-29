c24b18558da283bdae5987eefb28891aa3fd4f2c
Dmitry Berezovsky <dmitry.berezovsky@logicify.com>
2016-03-29 18:48:57 +0300

    specified LANG var

--- start-gunicorn.sh
+++ start-gunicorn.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 source /srv/virtenv/bin/activate
+
+export LANG=en_US.utf8
+
+if [ -z "${INSTALL_REQUIREMENTS+x}" ]; then
+  echo "Skip installing requirements"
+else
+  pip install -r requirements.txt
+fi
 
 gunicorn $APP_MODULE \
     --name $APP_PROCESS_NAME \
